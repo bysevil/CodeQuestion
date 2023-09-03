ee05e10ef7872168b32d7e535944fe84aba308b3
bysevil <bysevil@163.com>
2023-09-03 12:17:55 +0800

    0831

--- 0831/lc1731.c
+++ 0831/lc1731.c
@@ -2,13 +2,18 @@
 //https://leetcode.cn/problems/minimum-degree-of-a-connected-trio-in-a-graph/description/
 
 int minTrioDegree(int n,int** edges, int edgesSize, int* edgesColSize){
-    int hashtabs[n][n], num[n];
+    int** hashtabs = calloc(n, sizeof(int*));
+    for(int i = 0; i < n; i++){
+        hashtabs[i] = calloc(n,sizeof(int));
+    }
+    int* nums = calloc(n,sizeof(int));
+
     memset(hashtabs,0,sizeof(hashtabs));
-    memset(num,0,sizeof(num));
+    memset(nums,0,sizeof(nums));
     for(int i = 0; i < edgesSize; i++){
         int x = edges[i][0] - 1, y = edges[i][1] - 1;
         hashtabs[x][y] = hashtabs[y][x] = 1;
-        num[x]++;num[y]++;
+        nums[x]++;nums[y]++;
     }
 
     int ans = INT_MAX;
@@ -17,11 +22,18 @@ int minTrioDegree(int n,int** edges, int edgesSize, int* edgesColSize){
             if(hashtabs[i][j]){
                 for(int k = j + 1; k < n; k++){
                     if(hashtabs[j][k] && hashtabs[i][k]){
-                        ans = fmin(ans, num[i] + num[j] + num[k] - 6);
+                        ans = fmin(ans, nums[i] + nums[j] + nums[k] - 6);
                     }
                 }
             }
         }
     }
+
+    free(nums);
+    for(int i = 0; i < n; i++){
+        free(hashtabs[i]);
+    }
+    free(hashtabs);
+    
     return ans == INT_MAX ? -1 : ans;
 }
