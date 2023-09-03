871229f9d6aec4b6652b7afeeb040bda6bc08617
bysevil <bysevil@163.com>
2023-09-03 12:17:44 +0800

    0825

--- 0825/lc26.c
+++ 0825/lc26.c
@@ -1,12 +1,11 @@
 // https://leetcode.cn/problems/remove-duplicates-from-sorted-array/description/
 int removeDuplicates(int* nums, int numsSize){
-    int i = 0,j = 1;
-    while(j < numsSize) {
-        if(nums[j] != nums[i]){
-            nums[i+1] = nums[j];
-            i++;
+    int ans = 0;
+    for(int i = 1; i < numsSize; i++) {
+        if(nums[ans] != nums[i]){
+            nums[ans+1] = nums[i];
+            ans++;
         }
-        j++;
     }
-    return i + 1;
+    return ans + 1;
 }
