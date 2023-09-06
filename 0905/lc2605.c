fe54175da682ac85c78604e9000e2e4c5408daa2
bysevil <bysevil@163.com>
2023-09-06 11:44:50 +0800

    0905

--- 0905/lc2605.c
+++ 0905/lc2605.c
@@ -2,33 +2,21 @@
 
 int minNumber(int* nums1, int nums1Size, int* nums2, int nums2Size){
     int hashtabs1[10] = { 0 };
-    int hashtabs2[10] = { 0 };
 
-    for(int i = 0; i < nums1Size; i++) hashtabs1[nums1[i]]++;
+    int min1 = 10;
+    for(int i = 0; i < nums1Size; i++) {
+        hashtabs1[nums1[i]]++;
+        if (min1 > nums1[i]) min1 = nums1[i];
+    }
 
-    int flag = 10;
+    int flag = 10, min2 = 10;
     for(int i = 0; i < nums2Size; i++)
     {
         if(hashtabs1[nums2[i]] && flag > nums2[i]) flag = nums2[i];
-        else hashtabs2[nums2[i]]++;
+        else if(min2 > nums2[i]) min2 = nums2[i];
     }
 
     if(flag < 10) return flag;
-    else{
-        int a,b;
-        for(int i = 0; i < 10; i++){
-            if(hashtabs1[i] > 0) {
-                a = i;
-                break;
-            }
-        }
-        for(int i = 0; i < 10; i++){
-            if(hashtabs2[i] > 0) {
-                b = i;
-                break;
-            }
-        }
-        if(a > b) return b * 10 + a;
-        else return a * 10 + b;
-    }
+    else if(min1 > min2) return min2 * 10 + min1;
+    else return min1 * 10 + min2;
 }
