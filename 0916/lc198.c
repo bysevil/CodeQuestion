4fc6b99e08fb517511fce8deca5ab253a229ff9f
bysevil <bysevil@163.com>
2023-09-16 23:37:16 +0800

    0916

---
+++ 0916/lc198.c
@@ -0,0 +1,11 @@
+// https://leetcode.cn/problems/house-robber/?envType=daily-question&envId=2023-09-16
+
+int rob(int* nums, int numsSize){
+    if(numsSize == 1) return nums[0];
+    else if(nums[0] > nums[1]) nums[1] = nums[0];
+    for(int i = 2; i < numsSize; i++){
+        nums[i] += nums[i-2];
+        if(nums[i] < nums[i-1]) nums[i] = nums[i-1];
+    }
+    return nums[numsSize-1];
+}
