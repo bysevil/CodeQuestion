d31f379c4d4bb969f9ea4e60e43d595095fb6eda
bysevil <bysevil@163.com>
2023-09-23 20:57:05 +0800

    0916

--- 0916/lc198.c
+++ 0916/lc198.c
@@ -2,7 +2,7 @@
 
 int rob(int* nums, int numsSize){
     if(numsSize == 1) return nums[0];
-    else if(nums[0] > nums[1]) nums[1] = nums[0];
+    else if(nums[0] > nums[1])
     for(int i = 2; i < numsSize; i++){
         nums[i] += nums[i-2];
         if(nums[i] < nums[i-1]) nums[i] = nums[i-1];
